8c71ece563198e1ae188a3969106c2ca0e14cdf9
KrinTrungprakan <6658001356@student.chula.ac.th>
2024-02-12 15:32:50 +0700

    hw

---
+++ comprog wk 6.py
@@ -0,0 +1,67 @@
+import random
+
+class thaigen:
+    def __init__(self):
+        self.lastpos = None
+        
+    def get(self):
+        consonant = random.choice(thc)
+        self.lastpos = consonant[0]
+        return consonant[0]
+    
+    def get_answer(self):
+        for consonant in thc:
+            if consonant.startswith(self.lastpos):
+                split_entry = consonant
+                return [split_entry[0], split_entry[1], *split_entry[2:].split()]
+thc = [
+    "กMgor gai",
+    "ขHkho khai",
+    "ฃHkho khuat (not used)",
+    "คLkho khwai",
+    "ฅLkho khon (not used)",
+    "ฆLkho rakhang",
+    "งMngo ngu",
+    "จHjoor jaan",
+    "ฉHcho ching",
+    "ชMcho chang",
+    "ซLso so",
+    "ฌLso cheer",
+    "ญLyo ying",
+    "ฎMdo chada",
+    "ฏMto patak",
+    "ฐHtho than",
+    "ฑLtho montho",
+    "ฒLtho phuthao",
+    "ณLno nen",
+    "ดMdo dek",
+    "ตMto tao",
+    "ถHtho thung",
+    "ทLtho thahan",
+    "ธLtho thong",
+    "นLno nu",
+    "บMbo baimai",
+    "ปMpo pla",
+    "ผHpho phueng",
+    "ฝHfo fa",
+    "พLpho phan",
+    "ฟLfo fan",
+    "ภLpho sam-phao",
+    "มLmo ma",
+    "ยLyo yak",
+    "รLro rua",
+    "ลLlo ling",
+    "วLwo waen",
+    "ศHso sala",
+    "ษHso rusi",
+    "สHso suea",
+    "หHho hip",
+    "ฬLlo chula",
+    "อMo ang",
+    "ฮLho nokhuk"
+]
+
+gen = thaigen()
+consonant = gen.get()
+print("Random consonant:", consonant)
+print("Answer:", gen.get_answer())
